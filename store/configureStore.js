43515d80fd6a2f4008de587892fb0c32fde03352
ethan.nguyen <ethan.nguyens@gmail.com>
2017-08-03 16:58:59 +0100

    add store configuration

---
+++ store/configureStore.js
@@ -0,0 +1,22 @@
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from '../reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
+
+const configureStore = () => {
+  if (process.env.NODE_ENV === 'dev') {
+    return createStore(
+      rootReducer,
+      initialState,
+      applyMiddleware(thunk, reduxImmutableStateInvariant())
+    );
+  }
+
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk)
+  )
+};
+
+export default configureStore;
