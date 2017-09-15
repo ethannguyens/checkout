64566a864c07ae93a789327090ca3be204562f3c
ethan.nguyen <ethan.nguyens@gmail.com>
2017-09-15 09:18:08 +0100

    add Pudo Collection Point comp

---
+++ src/components/pudo/PudoNewCollectionPoint.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+class PudoNewCollectionPoint extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+
+  render() {
+
+  }
+}
